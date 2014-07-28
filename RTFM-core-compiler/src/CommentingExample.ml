254a91156af205d80a34191c0020015cbeda2473
Emil Fresk <emil.fresk@gmail.com>
2014-07-28 15:32:39 +0200

    Added a Doxygen-like commenting example

---
+++ RTFM-core-compiler/src/CommentingExample.ml
@@ -0,0 +1,60 @@
+(**
+ * @breif Exception definition for length mismatch errors.
+ *)
+exception LengthMismatch of string;;
+
+(**
+ * @breif     Merges two lists of the same length by the usage of the
+ *            function f.
+ * @details   Call tree:
+ *            merge f [a1; a2; ... an] [b1; b2; ... bn] => [f a1 b1; f a2 b2; ... f an bn]
+ *
+ * @param f   Function to apply to the lists.
+ * @param l1  First list.
+ * @param l2  Second list.
+ *
+ * @return    The merged list.
+ *)
+let rec merge f l1 l2 =
+  if List.length l1 = List.length l2 then
+    match l1, l2 with
+      | [], []             -> []
+      | h1 :: t1, h2 :: t2 -> f h1 h2 :: merge f t1 t2
+  else
+    raise (LengthMismatch "merge: Lists are of different lengths!")
+;;
+
+(**
+ * @breif     Converts a list of strings to a list of the lengths of the
+ *            corresponding strings.
+ *
+ * @param lst List of strings to convert.
+ *
+ * @return    The converted list.
+ *)
+let rec string_list_to_size lst =
+  match lst with
+    | []       -> []
+    | hd :: tl -> List.map String.length hd :: string_list_to_size tl
+;;
+
+(**
+ * @breif     Converts a list of lists to a single list.
+ * @details   Makes a list of lists and merges all of the lists to a single
+ *            list. The function f is applied on the two merging lists as
+ *            defined in @p merge.
+ *
+ *            Call tree:
+ *            merge_lists f [l1; l2; ... ln] => merge f ( ... merge f ( (merge f l1 l2) l3) ... ln )
+ *
+ * @param f   Function to apply to the lists while merging.
+ * @param lst List of lists to merge.
+ *
+ * @return     The merged list.
+ *)
+let rec merge_lists f lst =
+  match lst with
+    | []               -> lst
+    | _::[]            -> lst
+    | hd1 :: hd2 :: tl -> merge_lists f (merge f hd1 hd2 :: tl)
+;;
