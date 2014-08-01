9a74ee9e111aed0106fd013badcd9be07c604823
Per Lindgren <per.lindgren@ltu.se>
2014-08-02 01:20:41 +0200

    Additional commit

--- WINCORE/RTFM-RT/RTFM-RT.h
+++ WINCORE/RTFM-RT/RTFM-RT.h
@@ -1,17 +1,19 @@
 /*
- * RTFM-RT.h
- * WIN32 run-time system for RTFM-core
- * (C) 2014 Per Lindgren, Marcus Lindner
- */
+* RTFM-RT.h
+* WIN32 run-time for RTFM-core
+* (C) 2014 Per Lindgren, Marcus Lindner
+*/
+
 
 #ifndef RTFM_RT_H_
 #define RTFM_RT_H_
 
-typedef void(*ENTRY_FUNC)();
+typedef void(*ENTRY_FUNC)(int);
+void user_reset(int);
 
-void RTFM_lock(int r);
-void RTFM_unlock(int r);
-void RTFM_pend(int t);
+void RTFM_lock(int, int);
+void RTFM_unlock(int, int);
+void RTFM_pend(int,int);
 
-#define SEC(x)		(1000*x)
+#define SLEEP(x)		Sleep(1000*x)
 #endif /* RTFM_RT_H_ */
