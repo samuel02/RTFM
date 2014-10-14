1271767c035f80c1154d1ff7641a9cbce3edbc45
Samuel Nilsson <mail@samuelnilsson.me>
2014-10-14 09:15:21 +0200

    Move raising of typeerror to function

--- RTFM-cOOre-compiler/src/VariableTypeCheck.ml
+++ RTFM-cOOre-compiler/src/VariableTypeCheck.ml
@@ -10,6 +10,7 @@ exception TypeError of string
 exception NameError of string
 exception NotImplemented of string
 
+let raise_type_error msg = raise (TypeError("TypeError: " ^ msg));;
 
 type binding = id * pType
 
@@ -21,7 +22,7 @@ let rec type_of id env =
 let unify t1 t2 =
     if t1 == t2 then t1
     else
-        raise (TypeError(" Types not matching: arg1 = " ^ string_of_pType t1 ^ ", arg2 = " ^ string_of_pType t2 ^ ". "))
+        raise_type_error ("Types not matching: arg1 = " ^ string_of_pType t1 ^ ", arg2 = " ^ string_of_pType t2 ^ ". ")
 
 let in_list rt l =
     let equal rt t = rt = t in
@@ -30,20 +31,20 @@ let in_list rt l =
 let typecheck_op env op t1 t2 =
     let rt = unify t1 t2 in
     match op with
-    | OpPlus -> if in_list rt [Int]           then rt     else raise (TypeError("TypeError: + operator is not defined for type " ^ string_of_pType rt ^ "."))
-    | OpSub  -> if in_list rt [Int]           then rt     else raise (TypeError("TypeError: - operator is not defined for type " ^ string_of_pType rt ^ "."))
-    | OpMult -> if in_list rt [Int]           then rt     else raise (TypeError("TypeError: * operator is not defined for type " ^ string_of_pType rt ^ "."))
-    | OpDiv  -> if in_list rt [Int]           then rt     else raise (TypeError("TypeError: / operator is not defined for type " ^ string_of_pType rt ^ "."))
-    | OpMod  -> if in_list rt [Int]           then rt     else raise (TypeError("TypeError: % operator is not defined for type " ^ string_of_pType rt ^ "."))
-    | OpEq   -> if in_list rt [Int;Bool;Byte] then Bool   else raise (TypeError("Equality"))
-    | OpNeq  -> if in_list rt [Int;Bool;Byte] then Bool   else raise (TypeError("Non-equality"))
-    | OpGrt  -> if in_list rt [Int;Bool;Byte] then Bool   else raise (TypeError("Greater than"))
-    | OpGeq  -> if in_list rt [Int;Bool;Byte] then Bool   else raise (TypeError("Greater or equal"))
-    | OpLet  -> if in_list rt [Int;Bool;Byte] then Bool   else raise (TypeError("Less than"))
-    | OpLeq  -> if in_list rt [Int;Bool;Byte] then Bool   else raise (TypeError("Less or equal"))
+    | OpPlus -> if in_list rt [Int]           then rt     else raise_type_error ("+ operator is not defined for type " ^ string_of_pType rt ^ ".")
+    | OpSub  -> if in_list rt [Int]           then rt     else raise_type_error ("- operator is not defined for type " ^ string_of_pType rt ^ ".")
+    | OpMult -> if in_list rt [Int]           then rt     else raise_type_error ("* operator is not defined for type " ^ string_of_pType rt ^ ".")
+    | OpDiv  -> if in_list rt [Int]           then rt     else raise_type_error ("/ operator is not defined for type " ^ string_of_pType rt ^ ".")
+    | OpMod  -> if in_list rt [Int]           then rt     else raise_type_error ("% operator is not defined for type " ^ string_of_pType rt ^ ".")
+    | OpEq   -> if in_list rt [Int;Bool;Byte] then Bool   else raise_type_error ("Equality")
+    | OpNeq  -> if in_list rt [Int;Bool;Byte] then Bool   else raise_type_error ("Non-equality")
+    | OpGrt  -> if in_list rt [Int;Bool;Byte] then Bool   else raise_type_error ("Greater than")
+    | OpGeq  -> if in_list rt [Int;Bool;Byte] then Bool   else raise_type_error ("Greater or equal")
+    | OpLet  -> if in_list rt [Int;Bool;Byte] then Bool   else raise_type_error ("Less than")
+    | OpLeq  -> if in_list rt [Int;Bool;Byte] then Bool   else raise_type_error ("Less or equal")
 
 let rec typecheck_expr env = function
-    | IndexExp (idl, e)         -> if ((typecheck_expr env e) = Int) && ((type_of (List.nth idl ((List.length idl)-1)) env) = String) then Char else raise (TypeError("Incorrect string indexing"))
+    | IndexExp (idl, e)         -> if ((typecheck_expr env e) = Int) && ((type_of (List.nth idl ((List.length idl)-1)) env) = String) then Char else raise_type_error ("Incorrect string indexing")
     | MathExp (op, a, b)        -> typecheck_op env op (typecheck_expr env a) (typecheck_expr env b)
     | CompExp (op, e1, e2)      -> typecheck_op env op (typecheck_expr env e1) (typecheck_expr env e2)
     | ParExp (e)                -> typecheck_expr env e
@@ -66,10 +67,10 @@ let rec typecheck_stmt env =  function
     | RT_Sleep (e)      -> typecheck_expr env e; env
     | RT_Putc (e)       -> typecheck_expr env e; env
     | Else (s)          -> typecheck_stmt env s
-    | MPVar (t, i, e)   -> if typecheck_expr env e = t then (i, t)::env else raise (TypeError("TypeError: " ^ string_of_expr e ^ " is not of type " ^ string_of_pType t ^ "."))
-    | Assign (i, e)     -> if type_of i env = typecheck_expr env e then env else raise (TypeError("TypeError: Cannot assign " ^ string_of_pType (typecheck_expr env e) ^ " " ^ string_of_expr e ^ " to " ^ string_of_pType (type_of i env) ^ " " ^ i ^ "."))
-    | If (e, s)         -> if in_list (typecheck_expr env e) [Bool; Int] then typecheck_stmt env s else raise (TypeError("If: "^string_of_expr e))
-    | While (e, s)      -> if in_list (typecheck_expr env e) [Bool; Int] then typecheck_stmt env s else raise (TypeError("While: "^string_of_expr e))
+    | MPVar (t, i, e)   -> if typecheck_expr env e = t then (i, t)::env else raise_type_error (string_of_expr e ^ " is not of type " ^ string_of_pType t ^ ".")
+    | Assign (i, e)     -> if type_of i env = typecheck_expr env e then env else raise_type_error ("Cannot assign " ^ string_of_pType (typecheck_expr env e) ^ " " ^ string_of_expr e ^ " to " ^ string_of_pType (type_of i env) ^ " " ^ i ^ ".")
+    | If (e, s)         -> if in_list (typecheck_expr env e) [Bool; Int] then typecheck_stmt env s else raise_type_error ("If: "^string_of_expr e)
+    | While (e, s)      -> if in_list (typecheck_expr env e) [Bool; Int] then typecheck_stmt env s else raise_type_error ("While: "^string_of_expr e)
 
 let typecheck_classDecl env =
   let rec binding_argList env arg = match arg with
@@ -78,13 +79,13 @@ let typecheck_classDecl env =
     | []                        -> env
     in
     function
-    | CPVar (t, i, e)        -> if typecheck_expr env e = t then (i, t)::env else raise (TypeError("TypeError: " ^ string_of_expr e ^ " is not of type " ^ string_of_pType t ^ "."))
-    | COVar (o, el, i)       -> if (List.length (List.map (typecheck_expr env) el) >= 0) then (*(i, Void)::*)env else raise (TypeError(""))
-    | CMDecl (t, i, al, sl)  -> if (List.length (List.fold_left typecheck_stmt (binding_argList env al) sl) >= 0) then (*(i, t)::*)env else raise (TypeError(""))
-    | CTaskDecl (i, al, sl ) -> if (List.length (List.fold_left typecheck_stmt (binding_argList env al) sl) >= 0) then (*(i, Void)::*)env else raise (TypeError(""))
-    | CIsrDecl (pr, i, sl)   -> if (List.length (List.fold_left typecheck_stmt env sl)) >= 0 then env else raise (TypeError(""))
-    | CResetDecl (sl)        -> if (List.length (List.fold_left typecheck_stmt env sl)) >= 0 then env else raise (TypeError(""))
-    | CIdleDecl (sl)         -> if (List.length (List.fold_left typecheck_stmt env sl)) >= 0 then env else raise (TypeError(""))
+    | CPVar (t, i, e)        -> if typecheck_expr env e = t then (i, t)::env else raise_type_error (string_of_expr e ^ " is not of type " ^ string_of_pType t ^ ".")
+    | COVar (o, el, i)       -> if (List.length (List.map (typecheck_expr env) el) >= 0) then (*(i, Void)::*)env else raise_type_error ("")
+    | CMDecl (t, i, al, sl)  -> if (List.length (List.fold_left typecheck_stmt (binding_argList env al) sl) >= 0) then (*(i, t)::*)env else raise_type_error ("")
+    | CTaskDecl (i, al, sl ) -> if (List.length (List.fold_left typecheck_stmt (binding_argList env al) sl) >= 0) then (*(i, Void)::*)env else raise_type_error ("")
+    | CIsrDecl (pr, i, sl)   -> if (List.length (List.fold_left typecheck_stmt env sl)) >= 0 then env else raise_type_error ("")
+    | CResetDecl (sl)        -> if (List.length (List.fold_left typecheck_stmt env sl)) >= 0 then env else raise_type_error ("")
+    | CIdleDecl (sl)         -> if (List.length (List.fold_left typecheck_stmt env sl)) >= 0 then env else raise_type_error ("")
 
 let typecheck_classDef =
     let rec binding_classEnv = function
